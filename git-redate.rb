438cb21247c36093af841e93ab06973f84219ed4
fang duan <df1228@gmail.com>
2020-10-11 18:02:32 +0800

    Update git-redate.rb

--- git-redate.rb
+++ git-redate.rb
@@ -3,7 +3,7 @@ class GitRedate < Formula
   homepage "https://github.com/dfang/git-redate"
   url "https://github.com/dfang/git-redate/archive/0.1.tar.gz"
   version "0.1"
-  sha256 "970f07bb369211e0b366e07a07fac7a872a70d10"
+  sha256 "637fb8a9daaaee093533f45576574ed2294f4cc6dc1869d1daf8a76dea0519f9"
 
   def install
     bin.install "git-redate"
