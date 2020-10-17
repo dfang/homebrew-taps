f0abcd9e3b467faf4883d8e75add889063b90b37
fang duan <df1228@gmail.com>
2020-10-17 14:14:51 +0800

    Bump to version 0.2

--- git-redate.rb
+++ git-redate.rb
@@ -1,9 +1,9 @@
 class GitRedate < Formula
   desc "Change dates of git commits"
   homepage "https://github.com/dfang/git-redate"
-  url "https://github.com/dfang/git-redate/archive/0.1.tar.gz"
-  version "0.1"
-  sha256 "637fb8a9daaaee093533f45576574ed2294f4cc6dc1869d1daf8a76dea0519f9"
+  url "https://github.com/dfang/git-redate/archive/0.2.tar.gz"
+  version "0.2"
+  sha256 "b148a15a7e9d2c6584f7be0d20fb83af312cb7951a8603cc818df5b76a1878a3"
 
   def install
     bin.install "git-redate"
